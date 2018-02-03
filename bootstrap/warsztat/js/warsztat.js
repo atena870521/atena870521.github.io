d9e15a6f0e00839cac2c247963df45449b8dce49
Aneta <anetapierowicz@gmail.com>
2018-02-03 14:26:32 +0100

    bootstrap

---
+++ bootstrap/warsztat/js/warsztat.js
@@ -0,0 +1,19 @@
+//$(function(){
+//    
+//       if($(window).scrollTop() >= 50) {
+//          $('#main-menu').addClass('scroll');
+//       
+//          };
+//   /*scroll menu color change*/
+//   $(window).scroll(function(){
+//       if($(window).scrollTop() >= 50) {
+//          $('#main-menu').addClass('scroll');
+//           $('#main-menu').css({'transition':'all 0.3s'});
+//       } else {
+//           $('#main-menu').removeClass('scroll');
+//       }
+//          });
+//   });
+
+
+document.getElementsByTagName(‘section‘)[0].nodeType
